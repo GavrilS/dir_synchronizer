71df1c1f47316fd7b7d305e8c1b50bd8ec816632
GavrilS <gavr1lll@abv.bg>
2025-04-20 08:47:24 +0300

    Added checks for the provided paths

--- sync.py
+++ sync.py
@@ -3,19 +3,30 @@ import os
 class Synchronizer:
 
     def __init__(self, source, destination):
-        self._source = source
-        self._destination = destination
+        self.source = source
+        self.destination = destination
 
     @property
     def source(self):
         return self._source
     
+    @source.setter
+    def source(self, source):
+        if not os.path.isdir(source):
+            raise Exception('The provided source directory does not exist...')
+        
+        self._source = source
+    
     @property
     def destination(self):
         return self._destination
     
+    @destination.setter
+    def destinaiton(self, destination):
+        if not os.path.isdir(destination):
+            os.makedirs(destination)
+        
+        self._destination = destination
+    
     def sync_folders(self):
-        pass
-
-    def _check_paths(self, source, destination):
         pass
